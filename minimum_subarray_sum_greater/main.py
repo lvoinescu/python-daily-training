db707612471aaad057d69db6bc59b1549b455a41
lvoinescu <voinescu_lucian@yahoo.com>
2020-09-13 21:02:23 +0300

    Cleanup the solution for "the minimal length of a contiguous sub-array of which the sum ≥ s" problem

--- minimum_subarray_sum_greater/main.py
+++ minimum_subarray_sum_greater/main.py
@@ -10,18 +10,18 @@
 # Explanation: the sub-array [4,3] has the minimal length under the problem constraint.
 
 class Solution:
-    def min_sub_array_len(self, nums, s):
+    def min_sub_array_len(self, nums, minimum_sum):
         start = 0
         answer = 0
         while start < len(nums):
             partial_sum = 0
             i = start
             minimal_length = 0
-            while partial_sum < s and i < len(nums):
+            while partial_sum < minimum_sum and i < len(nums):
                 partial_sum += nums[i]
                 minimal_length = minimal_length + 1
                 i = i + 1
-            if partial_sum >= 0 and partial_sum >= s:
+            if partial_sum >= 0 and partial_sum >= minimum_sum:
                 print("Sum: " + str(partial_sum) + " for elements " + str(nums[start: i]) + " no: " + str(
                     len(nums[start:i])))
                 if answer == 0 or minimal_length < answer:
